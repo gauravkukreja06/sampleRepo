0bd0dacaeedac7cd7731d63c0f953dd76231aa29
gauravkukreja06 <gauravkukreja06@users.noreply.github.com>
2025-06-12 13:41:31 -0500

    Update db.sql

--- db.sql
+++ db.sql
@@ -1,7 +1,20 @@
+CREATE TABLE users
+( 
+  user_id NUMBER NOT NULL,
+  first_name VARCHAR2(200 BYTE) NULL,
+  last_name VARCHAR2(200 BYTE) NULL,
+  email VARCHAR2(200 BYTE) NULL
+);
+
+CREATE TABLE orders
+( 
+  order_id NUMBER NOT NULL,
+  product VARCHAR2(200 BYTE) NULL,
+  user_id NUMBER NOT NULL,
+);
+
 SELECT * FROM users;
 
-SELECT name, (SELECT COUNT(*) FROM orders WHERE user_id = users.id) AS order_count FROM users;
+SELECT first_name, last_name, (SELECT COUNT(*) FROM orders WHERE user_id = users.id) AS order_count FROM users;
 
-SELECT * FROM products WHERE category = 'fruit' OR category ='dairy';
-
-SELECT * FROM products WHERE product_name LIKE '%shirt';
+SELECT * FROM orders WHERE product = 'apple' OR product = 'banana' or product = 'orange' or product = 'pear';
